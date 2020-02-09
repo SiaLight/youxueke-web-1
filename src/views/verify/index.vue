3b4506da49a92a762cc757fac118de7bdd52c60d
Jasmine12LI <2273138399@qq.com>
2020-02-09 10:25:02 +0800

    ...

--- src/views/verify/index.vue
+++ src/views/verify/index.vue
@@ -13,16 +13,37 @@
     name: "index",
     components: { courseList },
     data: () => ({
-      courseData: []
+      courseData: [
+          {
+              id: 1,
+              title: '操作系统',
+              trueName: '张同学',
+              location: '理科大楼',
+              date: '2019-12-12',
+              verification: false,
+              img:'../assets/logo.png',
+              des:'速度滑冰  三人同行染色体和 素人特工任务提供'
+          },
+          {
+              id: 1,
+              title: '操作系统',
+              trueName: '张同学',
+              location: '理科大楼',
+              date: '2019-12-13',
+              verification: false,
+              img:'../assets/logo.png',
+              des:'速度滑冰  三人同行染色体和 素人特工任务提供'
+          }
+    ]
     }),
-    mounted () {
-      utils.request({
-        invoke: utils.api.allCourse
-      })
-        .then(res => {
-          this.courseData = res.Course.filter(item => !item.verification)
-        })
-    }
+    // mounted () {
+    //   utils.request({
+    //     invoke: utils.api.allCourse
+    //   })
+    //     .then(res => {
+    //       this.courseData = res.Course.filter(item => !item.verification)
+    //     })
+    // }
   }
 </script>
 
